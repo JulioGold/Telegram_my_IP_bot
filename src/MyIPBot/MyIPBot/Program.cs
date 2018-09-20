47c4036c50bcb4dc40467e0469919d10ebbb1500
JulioGold <julio.gold@gmail.com>
2018-09-20 20:57:12 -0300

    Added Authorization by ID.

--- src/MyIPBot/MyIPBot/Program.cs
+++ src/MyIPBot/MyIPBot/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
@@ -43,11 +44,18 @@ namespace MyIPBot
 
             switch (message.Text.Split(' ').First())
             {
+                case "/showmyid":
+                    Console.WriteLine($"The ID of {message.From.FirstName} is {message.From.Id }");
+                    await _bot.SendTextMessageAsync(message.Chat.Id, $"Your ID {message.From.FirstName} is: {message.From.Id}");
+                    break;
+
                 case "/myip":
+                    if (!await Authorized(message)) { break; }
                     await _bot.SendTextMessageAsync(message.Chat.Id, $"Your IP address is: {GetExternalIPAddress()}");
                     break;
 
                 case "/raport":
+                    if (!await Authorized(message)) { break; }
                     await _bot.SendTextMessageAsync(message.Chat.Id, "Use port 3389 to remote access.");
                     break;
 
@@ -61,6 +69,19 @@ Usage:
                     await _bot.SendTextMessageAsync(message.Chat.Id, usage, replyMarkup: new ReplyKeyboardRemove());
                     break;
             }
+        }
+
+        public static async Task<bool> Authorized(Telegram.Bot.Types.Message message)
+        {
+            var authorizedUsers = _config.GetSection("AuthorizedUsers").GetChildren().ToArray().Select(c => c.Value).ToList();
+
+            if (!authorizedUsers.Contains(message.From.Id.ToString()))
+            {
+                await _bot.SendTextMessageAsync(message.Chat.Id, "You have no access to it!", replyMarkup: new ReplyKeyboardRemove());
+                return false;
+            }
+
+            return true;
         }
 
         public static string GetExternalIPAddress()
